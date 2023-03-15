1af1afb5ba4ae06a3e7e5702c6b0f245e727c777
Cyrille Meichel <cmeichel@free.fr>
2023-03-15 13:48:18 +0100

    Initial commit

---
+++ config.js
@@ -0,0 +1,2 @@
+// First sheet (gid=0) contains on the first column, the list of all sheet gids
+document.url = 'https://docs.google.com/spreadsheets/d/120sxGhBvl2aFi_yky_dQpXFRr9VpMRxTBU9gol-ayho/export\?exportFormat\=csv'
